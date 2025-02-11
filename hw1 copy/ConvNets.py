11fece32dd1c35cc2d1819db552ab7bfc5e6f551
danielwang0452 <150024781+danielwang0452@users.noreply.github.com>
2025-02-11 22:52:56 +1100

    Add files via upload

---
+++ hw1 copy/ConvNets.py
@@ -0,0 +1,188 @@
+# As usual, a bit of setup
+
+import numpy as np
+import matplotlib.pyplot as plt
+from deeplearning.classifiers.cnn import *
+from deeplearning.data_utils import get_CIFAR10_data
+from deeplearning.gradient_check import eval_numerical_gradient_array, eval_numerical_gradient
+from deeplearning.layers import *
+from deeplearning.fast_layers import *
+from deeplearning.solver import Solver
+import random
+import torch
+
+seed = 7
+torch.manual_seed(seed)
+random.seed(seed)
+np.random.seed(seed)
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# for auto-reloading external modules
+# see http://stackoverflow.com/questions/1907993/autoreload-of-modules-in-ipython
+
+def rel_error(x, y):
+    """ returns relative error """
+    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+
+data = get_CIFAR10_data()
+# --------------------------------------------------------------------------------
+x_shape = (2, 3, 4, 4)
+w_shape = (3, 3, 4, 4)
+x = np.linspace(-0.1, 0.5, num=np.prod(x_shape)).reshape(x_shape)
+w = np.linspace(-0.2, 0.3, num=np.prod(w_shape)).reshape(w_shape)
+b = np.linspace(-0.1, 0.2, num=3)
+
+conv_param = {'stride': 2, 'pad': 1}
+out, _ = conv_forward_naive(x, w, b, conv_param)
+correct_out = np.array([[[[[-0.08759809, -0.10987781],
+                           [-0.18387192, -0.2109216 ]],
+                          [[ 0.21027089,  0.21661097],
+                           [ 0.22847626,  0.23004637]],
+                          [[ 0.50813986,  0.54309974],
+                           [ 0.64082444,  0.67101435]]],
+                         [[[-0.98053589, -1.03143541],
+                           [-1.19128892, -1.24695841]],
+                          [[ 0.69108355,  0.66880383],
+                           [ 0.59480972,  0.56776003]],
+                          [[ 2.36270298,  2.36904306],
+                           [ 2.38090835,  2.38247847]]]]])
+
+# Compare your output to ours; difference should be around 1e-8
+#print ('Testing conv_forward_naive')
+#print ('difference: ', rel_error(out, correct_out))
+
+x = np.random.randn(4, 3, 5, 5)
+w = np.random.randn(2, 3, 3, 3)
+b = np.random.randn(2,)
+dout = np.random.randn(4, 2, 5, 5)
+conv_param = {'stride': 1, 'pad': 1}
+
+dx_num = eval_numerical_gradient_array(lambda x: conv_forward_naive(x, w, b, conv_param)[0], x, dout)
+dw_num = eval_numerical_gradient_array(lambda w: conv_forward_naive(x, w, b, conv_param)[0], w, dout)
+db_num = eval_numerical_gradient_array(lambda b: conv_forward_naive(x, w, b, conv_param)[0], b, dout)
+
+out, cache = conv_forward_naive(x, w, b, conv_param)
+dx, dw, db = conv_backward_naive(dout, cache)
+
+# Your errors should be around 1e-9'
+#print ('Testing conv_backward_naive function')
+#print ('dx error: ', rel_error(dx, dx_num))
+#print ('dw error: ', rel_error(dw, dw_num))
+#print ('db error: ', rel_error(db, db_num))
+
+x_shape = (2, 3, 4, 4)
+x = np.linspace(-0.3, 0.4, num=np.prod(x_shape)).reshape(x_shape)
+pool_param = {'pool_width': 2, 'pool_height': 2, 'stride': 2}
+
+out, _ = max_pool_forward_naive(x, pool_param)
+
+correct_out = np.array([[[[-0.26315789, -0.24842105],
+                          [-0.20421053, -0.18947368]],
+                         [[-0.14526316, -0.13052632],
+                          [-0.08631579, -0.07157895]],
+                         [[-0.02736842, -0.01263158],
+                          [ 0.03157895,  0.04631579]]],
+                        [[[ 0.09052632,  0.10526316],
+                          [ 0.14947368,  0.16421053]],
+                         [[ 0.20842105,  0.22315789],
+                          [ 0.26736842,  0.28210526]],
+                         [[ 0.32631579,  0.34105263],
+                          [ 0.38526316,  0.4       ]]]])
+
+# Compare your output with ours. Difference should be around 1e-8.
+#print ('Testing max_pool_forward_naive function:')
+#print ('difference: ', rel_error(out, correct_out))
+
+x = np.random.randn(3, 2, 8, 8)
+dout = np.random.randn(3, 2, 4, 4)
+pool_param = {'pool_height': 2, 'pool_width': 2, 'stride': 2}
+
+dx_num = eval_numerical_gradient_array(lambda x: max_pool_forward_naive(x, pool_param)[0], x, dout)
+
+out, cache = max_pool_forward_naive(x, pool_param)
+dx = max_pool_backward_naive(dout, cache)
+
+# Your error should be around 1e-12
+# print ('Testing max_pool_backward_naive function:')
+# print ('dx error: ', rel_error(dx, dx_num))
+
+model = ThreeLayerConvNet(num_filters=3, filter_size=1)
+
+N = 50
+X = np.random.randn(N, 3, 32, 32)
+y = np.random.randint(10, size=N)
+
+num_train = 100
+small_data = {
+  'X_train': data['X_train'][:num_train],
+  'y_train': data['y_train'][:num_train],
+  'X_val': data['X_val'],
+  'y_val': data['y_val'],
+}
+# test fast layers
+import pyximport
+pyximport.install()
+
+from deeplearning.fast_layers import conv_forward_fast, conv_backward_fast
+from time import time
+
+x = np.random.randn(100, 3, 31, 31)
+w = np.random.randn(25, 3, 3, 3)
+b = np.random.randn(25,)
+dout = np.random.randn(100, 25, 16, 16)
+conv_param = {'stride': 2, 'pad': 1}
+
+t0 = time()
+out_naive, cache_naive = conv_forward_naive(x, w, b, conv_param)
+t1 = time()
+out_fast, cache_fast = conv_forward_fast(x, w, b, conv_param)
+t2 = time()
+
+print ('Testing conv_forward_fast:')
+print ('Naive: %fs' % (t1 - t0))
+print ('Fast: %fs' % (t2 - t1))
+print ('Speedup: %fx' % ((t1 - t0) / (t2 - t1)))
+print ('Difference: ', rel_error(out_naive, out_fast))
+
+t0 = time()
+dx_naive, dw_naive, db_naive = conv_backward_naive(dout, cache_naive)
+t1 = time()
+dx_fast, dw_fast, db_fast = conv_backward_fast(dout, cache_fast)
+t2 = time()
+
+print ('\nTesting conv_backward_fast:')
+print ('Naive: %fs' % (t1 - t0))
+print ('Fast: %fs' % (t2 - t1))
+print ('Speedup: %fx' % ((t1 - t0) / (t2 - t1)))
+print ('dx difference: ', rel_error(dx_naive, dx_fast))
+print ('dw difference: ', rel_error(dw_naive, dw_fast))
+print ('db difference: ', rel_error(db_naive, db_fast))
+# train model
+model = ThreeLayerConvNet(weight_scale=1e-2)
+
+solver = Solver(model, small_data,
+                num_epochs=20, batch_size=50,
+                update_rule='adam',
+                optim_config={
+                  'learning_rate': 1e-3,
+                },
+                verbose=True, print_every=1)
+solver.train()
+
+plt.subplot(2, 1, 1)
+plt.plot(solver.loss_history, 'o')
+plt.xlabel('iteration')
+plt.ylabel('loss')
+
+plt.subplot(2, 1, 2)
+plt.plot(solver.train_acc_history, '-o')
+plt.plot(solver.val_acc_history, '-o')
+plt.legend(['train', 'val'], loc='upper left')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.show()
+
+solver.record_histories_as_npz('submission_logs/overfit_3layer_cnn.npz')
